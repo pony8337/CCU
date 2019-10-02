cdefb6cd5f742e26d0faa62be22e8557624a70ad
pony8337 <jeffary12@gmail.com>
2019-10-02 23:41:47 +0800

    [Software Test] Update HW2 function.
    PS. the last version issue is not error, it is only warning.

--- Software Test/HW2/test/GradeConverterTest.java
+++ Software Test/HW2/test/GradeConverterTest.java
@@ -2,10 +2,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class GradeConverterTest {
+import junit.framework.TestCase;
+
+class GradeConverterTest extends TestCase {
 
 	@Test
 	public void testGradeConverter() {
+      // test GradeConverter construction
 	  GradeConverter g1 = new GradeConverter();
 	  assertNotNull(g1);	
 	}
@@ -13,10 +16,20 @@ class GradeConverterTest {
 	@Test
 	public void testConvert() {
 	  GradeConverter g1 = new GradeConverter();
+	  String test;
+	  // Test convert method
 	  try {
-	    assertEquals("There is a bug in convert function of GradeConverter!", "A", g1.convert(100));
+		test = g1.convert(100);
+	    assertEquals("There is a bug in convert function of GradeConverter!", "A", test);
 	  } catch(IllegalGradeException e) {
-		assertTrue(false, "No exception should be thrown.");
+		assertTrue("No exception should be thrown.", false);
+	  }
+	  // Test exception of convert method
+	  try {
+		test = g1.convert(111);
+		assertTrue("There should be a exception.", false);
+	  } catch(IllegalGradeException e) {
+		assertTrue(true);
 	  }
 	}
 
